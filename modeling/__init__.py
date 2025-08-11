628ecd7a78bfb18f72e4918c5578a9f00fe08de4
Md Ikram <86195681+kaziikram678@users.noreply.github.com>
2025-08-12 03:05:59 +0600

    Update __init__.py

--- modeling/__init__.py
+++ modeling/__init__.py
@@ -1,14 +1,20 @@
-# encoding: utf-8
-"""
-@author:  sherlock
-@contact: sherlockliao01@gmail.com
-"""
-
 from .baseline import Baseline
 
-
 def build_model(cfg, num_classes):
-    # if cfg.MODEL.NAME == 'resnet50':
-    #     model = Baseline(num_classes, cfg.MODEL.LAST_STRIDE, cfg.MODEL.PRETRAIN_PATH, cfg.MODEL.NECK, cfg.TEST.NECK_FEAT)
-    model = Baseline(num_classes, cfg.MODEL.LAST_STRIDE, cfg.MODEL.PRETRAIN_PATH, cfg.MODEL.NECK, cfg.TEST.NECK_FEAT, cfg.MODEL.NAME, cfg.MODEL.PRETRAIN_CHOICE)
+    use_sa_l3 = cfg.MODEL.SELF_ATTN in ('layer3', 'both')
+    use_sa_l4 = cfg.MODEL.SELF_ATTN in ('layer4', 'both')
+    use_sg_l3 = cfg.MODEL.SGCONV   in ('layer3', 'both')
+    use_sg_l4 = cfg.MODEL.SGCONV   in ('layer4', 'both')
+
+    model = Baseline(
+        num_classes,
+        last_stride=cfg.MODEL.LAST_STRIDE,
+        model_path=cfg.MODEL.PRETRAIN_PATH,
+        neck=cfg.MODEL.NECK,
+        neck_feat=cfg.TEST.NECK_FEAT,
+        model_name=cfg.MODEL.NAME,
+        pretrain_choice=cfg.MODEL.PRETRAIN_CHOICE,
+        use_sa_l3=use_sa_l3, use_sa_l4=use_sa_l4,
+        use_sg_l3=use_sg_l3, use_sg_l4=use_sg_l4,
+    )
     return model
